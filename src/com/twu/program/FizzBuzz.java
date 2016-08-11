c9fe3205f0cf04284c36f4af86f53443dd702dd0
Zhiyong Liu <zyongliu@thoughtworks.com>
2016-08-11 11:11:47 +0800

    FizzBuzz

---
+++ src/com/twu/program/FizzBuzz.java
@@ -0,0 +1,19 @@
+package com.twu.program;
+
+/**
+ * Created by zyongliu on 16/8/11.
+ */
+public class FizzBuzz {
+    public static void fizzBuzz( int n ){
+        for( int i=1; i<=n; i++ ) {
+            if (i % 3 != 0 && i % 5 != 0) System.out.print(i);
+            if (i % 3 == 0) System.out.print("Fizz");
+            if (i % 5 == 0) System.out.print("Buzz");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        FizzBuzz.fizzBuzz(100);
+    }
+}
